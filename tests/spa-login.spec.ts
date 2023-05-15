0604204e334614602dafda2a0167ddae23f5f5e9
Toz1k <tozik1994@gmail.com>
2023-05-15 16:51:43 +0300

    Added new test for SPA login

---
+++ tests/spa-login.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('login on the Prod UA SPA project', async ({ page, context }) => {
+  // Відкриваємо сайт
+  await page.goto('https://pin-up.ua/');
+  await page.waitForTimeout(3_000);
+
+  // Відкриваємо форму логіну 
+  await page.locator('[data-testid="loginBtn"]').isVisible();
+  await page.locator('[data-testid="loginBtn"]').click();
+
+  // Заповнюємо поля логіну та паролю
+  await page.locator('[data-testid="loginFormAuthInput"]').fill('test3@tobolin.net');
+  await page.locator('[data-testid="loginFormPasswordInput"]').fill('Pinup2013');  
+
+  // Тицяємо на кнопку "Log In"
+  await page.locator('[data-testid="loginBtn"]').isVisible();
+  await page.locator('[data-testid="loginBtn"]').click();
+
+  // Перевіряємо чи є кнопки "Профіль" та "Каса" на новій сторінці, щоб переконатись що ми дійсно залогінились
+  await page.locator('[data-testid="profileBtn"]').isVisible();
+  await page.locator('[data-testid="openCashboxBtn"]').isVisible();
+  
+});
